a13c2a1dc323606eaff48a76dfb1bfc38edd0f2b
kuznetsovsergey@ymail.com <github123869p>
2020-04-27 20:33:52 +0300

    structure refactoring

---
+++ src/main/java/kss/springframework/beer/order/service/config/JMSConfig.java
@@ -0,0 +1,20 @@
+package kss.springframework.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JMSConfig {
+
+    public static final String MYQUEUE = "my-hello-world";
+    @Bean
+    public MessageConverter getMessageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
